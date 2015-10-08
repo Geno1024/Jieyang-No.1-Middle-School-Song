56da9d080db8a7dd2474e45b36aae32f8a31d878
Geno1024 <754097987@qq.com>
2015-10-09 01:17:55 +0800

    [update] Basic structure.

--- src/JNMS_Song.java
+++ src/JNMS_Song.java
@@ -4,7 +4,13 @@ public class JNMS_Song
 {
 	public static void main(String[] args)
 	{
+		String prelude = "";
+		String part1 = "";
+		String part2bass = "";
+		String part2treble = "";
+		String interlude = "";
+		String postlude = "";
 		Player p = new Player();
-		p.play("");
+		p.play(prelude + part1 + part2bass + part2treble + interlude + part1 + part2bass + part2treble + postlude);
 	}
 }
