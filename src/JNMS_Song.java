eca0d725aa1ad9db69b445ddaf051a5c7828cdbd
Geno1024 <754097987@qq.com>
2015-10-18 12:23:07 +0800

    [update] add part2 bass

--- src/JNMS_Song.java
+++ src/JNMS_Song.java
@@ -1,3 +1,4 @@
+import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
 
 public class JNMS_Song
@@ -16,59 +17,89 @@ public class JNMS_Song
 	 */
 	public static void main(String[] args)
 	{
-		String prelude = "E6s. C6s. E6s. G6i. G6s | " +
-				"G6w | " +
-				"D6i Bi D6i Gi. Gs | " +
-				"Gi D6i E6i F6i | " +
-				"G6i G6 E6i | " +
-				"C6s. Gs. C6s. E6 | " +
-				"G6i G6 E6i | " +
-				"C6s. Gs. C6s. E6 | " +
-				"C6i. C6s Ai Fs As | " +
-				"Gi G Di | " +
-				"Bi Gi Bi D6i | " +
-				"C6 0 | ";
-		String part1 = "Gq. Ei | " +
-				"A G | " +
-				"C6q. D6i | " +
-				"C6 G | " +
-				"| " +
-				"A Ai Bi | " +
-				"C6 Bi Ai | " +
-				"G E6 D6w | " +
-				"| " +
-				"Gi. Gs Gi Ei | " +
-				"A G | " +
-				"C6i. C6s C6i D6i | " +
-				"C6 G | " +
-				"Ai Ai 0 | " +
-				"F6i F6i 0 | " +
-				"A6i A6i A6i G6i | " +
-				"F6i F6i E6i D6i | " +
-				"E6i E6i F6i 0i | " +
-				"G6q ";
-		String part2treble = "C6 | " +
-				"A6w | " +
-				"A6q F6i. A6s | " +
-				"G6w | " +
-				"G6 G | " +
-				"E6w | " +
-				"E6 C6i. E6s | " +
-				"D6w | " +
-				"D6 0 | " +
-				"E6i. E6s E6i D6i | " +
-				"C6 A | " +
-				"G6i. G6s G6i F6i | " +
-				"E6 D6 | " +
-				"C6i C6i Bi Ai | " +
-				"D6q. E6i | " +
-				"G6q. F6i | " +
-				"E6i E6i D6 | " +
-				"C6 0 | ";
-		String part2bass = "";
+		String prelude = "E6s. C6s. E6s. G6i. G6s | " +	// 3 1 3 5 5
+				"G6w | " +								// 5
+				"D6i Bi D6i Gi. Gs | " +				// 2 7 2 5 5
+				"Gi D6i E6i F6i | " +					// 5 2 3 4
+				"G6i G6 E6i | " +						// 5 5 3
+				"C6s. Gs. C6s. E6 | " +					// 1 5 1 3
+				"G6i G6 E6i | " +						// 5 5 3
+				"C6s. Gs. C6s. E6 | " +					// 1 5 1 3
+				"C6i. C6s Ai Fs As | " +				// 1 1 6 4 6
+				"Gi G Di | " +							// 5 5 2
+				"Bi Gi Bi D6i | " +						// 7 5 7 2
+				"C6 0 | ";								// 1 0
+		String part1 = "Gq. Ei | " +					// 5 3
+				"A G | " +								// 6 5
+				"C6q. D6i | " +							// 1 2
+				"C6 G | " +								// 1 5
+				"| " +									// Sentence break
+				"A Ai Bi | " +							// 6 6 7
+				"C6 Bi Ai | " +							// 1 7 6
+				"G E6 | " +								// 5 3
+				"D6w | " +								// 2
+				"| " +									// Sentence break
+				"Gi. Gs Gi Ei | " +						// 5 5 5 3
+				"A G | " +								// 6 5
+				"| " +									// Sentence break
+				"C6i. C6s C6i D6i | " +					// 1 1 1 2
+				"C6 G | " +								// 1 5
+				"| "  +									// Sentence break
+				"Ai Ai 0 | " +							// 6 6
+				"F6i F6i 0 | " +						// 4 4
+				"| " +									// Sentence break
+				"A6i A6i A6i G6i | " +					// 6 6 6 5
+				"| " +									// Sentence break
+				"F6i F6i E6i D6i | " +					// 4 4 3 2
+				"E6i E6i F6i 0i | " +					// 3 3 4 0
+				"G6q ";									// 5
+		String part2treble = "C6 | " +					// 1
+				"A6w | " +								// 6
+				"A6q F6i. A6s | " +						// 6 4 6
+				"G6w | " +								// 5
+				"| " +									// Sentence break
+				"G6 G | " +								// 5 5
+				"E6w | " +								// 3
+				"E6 C6i. E6s | " +						// 3 1 3
+				"D6w | " +								// 2
+				"| " +									// Sentence break
+				"D6 0 | " +								// 2 0
+				"E6i. E6s E6i D6i | " +					// 3 3 3 2
+				"C6 A | " +								// 1 6
+				"| " +									// Sentence break
+				"G6i. G6s G6i F6i | " +					// 5 5 5 4
+				"E6 D6 | " +							// 3 2
+				"| " +									// Sentence break
+				"C6i C6i Bi Ai | " +					// 1 1 7 6
+				"D6q. E6i | " +							// 2 3
+				"G6q. F6i | " +							// 5 4
+				"E6i E6i D6 | " +						// 3 3 2
+				"C6 0 | ";								// 1 0
+		String part2bass = "C6 | " +					// 1
+				"F6w | " +								// 4
+				"F6q D6i. F6s | " +						// 4 2 4
+				"E6w | " +								// 3
+				"| " +									// Sentence break
+				"E6 G | " +								// 3 5
+				"C6w | " +								// 1
+				"C6 Ai. C6s | " +						// 1 6 1
+				"B A | " +								// 7 6
+				"| " +									// Sentence break
+				"G 0 | " +								// 5 0
+				"C6i. C6s C6i Bi | " +					// 1 1 1 7
+				"A A | " +								// 6 6
+				"| " +									// Sentence break
+				"E6i. E6s E6i D6i | " +					// 3 3 3 2
+				"C6 B | " +								// 1 7
+				"| " +									// Sentence break
+				"Ai Ai Gi Fi | " +						// 6 6 5 4
+				"Aq. C6i | " +							// 6 1
+				"E6q. D6i | " +							// 3 2
+				"C6i C6i B | " +						// 1 1 7
+				"C6 0 | ";								// 1 0
 		String interlude = "";
 		String postlude = "";
 		Player p = new Player();
-		p.play(prelude + part1 + part2treble/* + part2bass + interlude + part1 + part2treble + part2bass + postlude*/);
+		p.play(/*prelude + part1 + */part2treble/* + part2bass + interlude + part1 + part2treble + part2bass + postlude*/);
 	}
 }
