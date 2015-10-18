a3514804e68b66a2546a511a62d4b933935ed42b
Geno1024 <754097987@qq.com>
2015-10-18 11:10:46 +0800

    [update] finish part 1?

--- src/JNMS_Song.java
+++ src/JNMS_Song.java
@@ -16,31 +16,59 @@ public class JNMS_Song
 	 */
 	public static void main(String[] args)
 	{
-		String prelude = "E6s. C6s. E6s. G6i. G6s " +
-				"G6w " +
-				"D6i Bi D6i Gi. Gs " +
-				"Gi D6i E6i F6i " +
-				"G6i G6 E6i " +
-				"C6s. Gs. C6s. E6 " +
-				"G6i G6 E6i " +
-				"C6s. Gs. C6s. E6 " +
-				"C6i. C6s Ai Fs As " +
-				"Gi G Di " +
-				"Bi Gi Bi D6i " +
-				"C6 0";
-		String part1 = "G. Ei " +
-				"A G " +
-				"C6. D6i " +
-				"C6 G " +
-				"" +
-				"A Ai Bi " +
-				"C6 Bi Ai " +
-				"G E6 D6w ";
+		String prelude = "E6s. C6s. E6s. G6i. G6s | " +
+				"G6w | " +
+				"D6i Bi D6i Gi. Gs | " +
+				"Gi D6i E6i F6i | " +
+				"G6i G6 E6i | " +
+				"C6s. Gs. C6s. E6 | " +
+				"G6i G6 E6i | " +
+				"C6s. Gs. C6s. E6 | " +
+				"C6i. C6s Ai Fs As | " +
+				"Gi G Di | " +
+				"Bi Gi Bi D6i | " +
+				"C6 0 | ";
+		String part1 = "Gq. Ei | " +
+				"A G | " +
+				"C6q. D6i | " +
+				"C6 G | " +
+				"| " +
+				"A Ai Bi | " +
+				"C6 Bi Ai | " +
+				"G E6 D6w | " +
+				"| " +
+				"Gi. Gs Gi Ei | " +
+				"A G | " +
+				"C6i. C6s C6i D6i | " +
+				"C6 G | " +
+				"Ai Ai 0 | " +
+				"F6i F6i 0 | " +
+				"A6i A6i A6i G6i | " +
+				"F6i F6i E6i D6i | " +
+				"E6i E6i F6i 0i | " +
+				"G6q ";
+		String part2treble = "C6 | " +
+				"A6w | " +
+				"A6q F6i. A6s | " +
+				"G6w | " +
+				"G6 G | " +
+				"E6w | " +
+				"E6 C6i. E6s | " +
+				"D6w | " +
+				"D6 0 | " +
+				"E6i. E6s E6i D6i | " +
+				"C6 A | " +
+				"G6i. G6s G6i F6i | " +
+				"E6 D6 | " +
+				"C6i C6i Gi Fi | " +
+				"D6q. E6i | " +
+				"G6q. F6i | " +
+				"E6i E6i D6 | " +
+				"C6 0 | ";
 		String part2bass = "";
-		String part2treble = "";
 		String interlude = "";
 		String postlude = "";
 		Player p = new Player();
-		p.play(/*prelude + */part1/* + part2bass + part2treble + interlude + part1 + part2bass + part2treble + postlude*/);
+		p.play(prelude + part1 + part2treble/* + part2bass + interlude + part1 + part2treble + part2bass + postlude*/);
 	}
 }
